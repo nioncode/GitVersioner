219eeba63dba76d92d4544c49b4a0da1ea16ce7d
Nicolas Schneider <nioncode+git@gmail.com>
2016-02-13 16:22:43 +0100

    remove optional arguments from constructors in tests

--- test/test.py
+++ test/test.py
@@ -20,39 +20,36 @@ class ParsingTestCase(unittest.TestCase):
         self.assertRaises(ValueError, lambda: Version.parse_from(''))
 
     def test_hash_only(self):
-        self.assertEquals(Version(0, 0, 0, None, "abcdefg", 0, False), Version.parse_from("abcdefg"))
+        self.assertEquals(Version(0, 0, commit_hash="abcdefg"), Version.parse_from("abcdefg"))
 
     def test_hash_only_dirty(self):
-        self.assertEquals(Version(0, 0, 0, None, "abcdefg", 0, True), Version.parse_from("abcdefg-dirty"))
+        self.assertEquals(Version(0, 0, commit_hash="abcdefg", is_dirty=True), Version.parse_from("abcdefg-dirty"))
 
     def test_tag_only(self):
-        self.assertEquals(Version(1, 2, 3, None, None, 0, False), Version.parse_from("v1.2.3"))
-        self.assertEquals(Version(1, 2, 3, None, None, 0, False), Version.parse_from("1.2.3"))
+        self.assertEquals(Version(1, 2, 3), Version.parse_from("v1.2.3"))
+        self.assertEquals(Version(1, 2, 3), Version.parse_from("1.2.3"))
 
     def test_tag_only_dirty(self):
-        self.assertEquals(Version(1, 2, 3, None, None, 0, True), Version.parse_from("v1.2.3-dirty"))
-        self.assertEquals(Version(1, 2, 3, None, None, 0, True), Version.parse_from("1.2.3-dirty"))
+        self.assertEquals(Version(1, 2, 3, is_dirty=True), Version.parse_from("v1.2.3-dirty"))
+        self.assertEquals(Version(1, 2, 3, is_dirty=True), Version.parse_from("1.2.3-dirty"))
 
     def test_tag_only_no_patch(self):
-        self.assertEquals(Version(1, 2, 0, None, None, 0, False), Version.parse_from("v1.2"))
-        self.assertEquals(Version(1, 2, 0, None, None, 0, False), Version.parse_from("1.2"))
+        self.assertEquals(Version(1, 2, 0), Version.parse_from("v1.2"))
+        self.assertEquals(Version(1, 2, 0), Version.parse_from("1.2"))
 
     def test_tag_only_no_patch_dirty(self):
-        self.assertEquals(Version(1, 2, 0, None, None, 0, True), Version.parse_from("v1.2-dirty"))
-        self.assertEquals(Version(1, 2, 0, None, None, 0, True), Version.parse_from("1.2-dirty"))
+        self.assertEquals(Version(1, 2, 0, is_dirty=True), Version.parse_from("v1.2-dirty"))
+        self.assertEquals(Version(1, 2, 0, is_dirty=True), Version.parse_from("1.2-dirty"))
 
     def test_commits_since(self):
         self.assertEquals(Version(1, 2, 0, None, "296cf8b", 12, False), Version.parse_from("v1.2.0-12-g296cf8b"))
         self.assertEquals(Version(1, 2, 0, None, "296cf8b", 12, True), Version.parse_from("v1.2.0-12-g296cf8b-dirty"))
 
     def test_pre_release_ids(self):
-        self.assertEquals(Version(1, 2, 3, "alpha.3", None, 0, False), Version.parse_from("1.2.3-alpha.3"))
-        self.assertEquals(Version(1, 2, 3, "alpha.3-special", None, 0, False),
-                          Version.parse_from("1.2.3-alpha.3-special"))
-        self.assertEquals(Version(1, 2, 3, "alpha.3-more-special", None, 0, False),
-                          Version.parse_from("1.2.3-alpha.3-more-special"))
-        self.assertEquals(Version(1, 2, 3, "alpha.3", None, 0, True),
-                          Version.parse_from("v1.2.3-alpha.3-dirty"))
+        self.assertEquals(Version(1, 2, 3, "alpha.3"), Version.parse_from("1.2.3-alpha.3"))
+        self.assertEquals(Version(1, 2, 3, "alpha.3-special"), Version.parse_from("1.2.3-alpha.3-special"))
+        self.assertEquals(Version(1, 2, 3, "alpha.3-more-special"), Version.parse_from("1.2.3-alpha.3-more-special"))
+        self.assertEquals(Version(1, 2, 3, "alpha.3", None, 0, True), Version.parse_from("v1.2.3-alpha.3-dirty"))
         self.assertEquals(Version(1, 2, 3, "alpha.3", "296cf8b", 12, True),
                           Version.parse_from("v1.2.3-alpha.3-12-g296cf8b-dirty"))
 
